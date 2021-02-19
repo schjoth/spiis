50e265a252d87af4b86cbb52efd237887dc2943f
André Schjøth <andschj@stud.ntnu.no>
2021-02-19 13:45:03 +0100

    feat(#29): la til side for å se brukerinfo, i tillegg til å legge opp for kobling opp mot tjener

---
+++ client/src/views/User.vue
@@ -0,0 +1,25 @@
+<template>
+  <Profile></Profile>
+</template>
+
+<script>
+import Profile from "../components/Profile";
+export default {
+  name: "profile",
+  components: {
+    Profile
+  },
+  data: () => {
+    return {
+      user: {
+        firstname: "Ola",
+        lastname: "Nordmann",
+        email: "ola_nordmann@gmail.com",
+        postcode: "7069, Trondheim",
+        allergies: "Gluten"
+      },
+      isMyUser: false
+    };
+  }
+};
+</script>
