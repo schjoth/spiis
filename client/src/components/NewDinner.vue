a32dd58c52b785dea1cdd1b25506209871e536ea
haved <krogstie.havard@gmail.com>
2021-02-20 17:36:34 +0100

    feat(#37): bruke nytt layout på opprett middag

--- client/src/components/NewDinner.vue
+++ client/src/components/NewDinner.vue
@@ -1,27 +1,80 @@
 <template>
-  <main>
+  <article class="max-600">
     <h1>Inviter til middag!</h1>
-    <label>Tittel:<input type="text" id="title" name="tittel" /></label>
-    <label>Tidspunkt:<input type="date" id="time" name="tidspunkt" /></label>
-    <label>Sted:<input type="text" id="location" name="sted" /></label>
-    <label
-      >Plasser:<input type="number" id="guests" name="plasser" min="1"
-    /></label>
-    <label
-      >Beskrivelse:<textarea
-        id="description"
-        name="beskrivelse"
-        rows="4"
-        cols="50"
-      />
-    </label>
-    <button class="is-primary">Opprett arrangement</button>
-  </main>
+    <div class="field">
+      <label class="label">Tittel</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="" v-model="input.title" />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Beskrivelse</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="" v-model="input.description" />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Sted</label>
+      <div class="control">
+        <input
+          class="input"
+          type="text"
+          placeholder=""
+          v-model="input.location"
+        />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Maks deltagere</label>
+      <div class="control">
+        <input
+          class="input"
+          type="number"
+          placeholder=""
+          v-model="input.maxGuests"
+        />
+      </div>
+    </div>
+    <div class="content has-text-centered" v-if="errorMessage">
+      <p class="has-text-danger">
+        {{ errorMessage }}
+      </p>
+    </div>
+    <div class="field is-grouped is-grouped-centered">
+      <div class="control">
+        <button class="button is-primary" v-on:click="createClicked">
+          Opprett arrangement!
+        </button>
+      </div>
+    </div>
+  </article>
 </template>
 
 <script>
+import { ref, reactive } from "vue";
+
 export default {
-  name: "NewDinner"
+  name: "NewDinner",
+
+  setup() {
+    const input = reactive({
+      title: "",
+      description: "",
+      location: "",
+      maxGuests: ""
+    });
+    const errorMessage = ref("");
+
+    const createClicked = () => {
+      //TODO
+    };
+
+    return {
+      input,
+      errorMessage,
+      createClicked
+    };
+  }
 };
 </script>
 
