19386a9b5047789df89d80ed650f047d275fa0f2
perolavbroback <perobro@stud.ntnu.no>
2021-02-22 14:02:41 +0100

    feat(#15): la til informasjon i middager

--- client/src/views/DinnerEvent.vue
+++ client/src/views/DinnerEvent.vue
@@ -1,21 +1,31 @@
 <template>
   <article>
+    <h1>dinner.title</h1>
     <p>id: {{ $route.params.dinnerId }}</p>
     <p>Denne siden vil ikke vise noe den før koblet til databasen</p>
-    <!-- <h1>{{ dinner.title }}</h1>
-    <p><b>Antall gjester:</b> {{ dinner.guests }}/{{ dinner.maxGuests }}</p>
-    <p><b>Sted:</b> {{ dinner.location }}</p>
-    <p><b>Beskrivelse: </b> {{ dinner.description }}</p>
-    -->
+    <p><b>Antall gjester:</b> dinner.guests / dinner.maxGuests</p>
+    <ul>
+      <li>dinner.users</li>
+    </ul>
+    <p><b>Sted:</b> dinner.location</p>
+    <p><b>Beskrivelse: </b> dinner.description</p>
     <router-link :to="'/event/' + $route.params.dinnerId + '/edit'">
       rediger
     </router-link>
+    <button type="button" v-on:click="registrerTilMiddag">
+      Meld deg på middagen.
+    </button>
   </article>
 </template>
 
 <script lang="ts">
 export default {
-  name: "DinnerEvent"
+  name: "DinnerEvent",
   //TODO hent middag som har id $route.params.dinnerId )
+  setup() {
+    const registrerTilMiddag = () => {
+      //TODO MELD BRUKER PÅ ARRANGEMENT
+    };
+  }
 };
 </script>
