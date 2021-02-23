526f30436e9b62e72da266878ac645d6faf0aefe
André Schjøth <andschj@stud.ntnu.no>
2021-02-24 00:36:15 +0100

    feat(28): la til input for allergener, som nå vises på profil

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
-    <p><b>Fornavn:</b> {{ user.firstName }}</p>
-    <p><b>Etternavn:</b> {{ user.lastName }}</p>
-    <p><b>Alder:</b> {{ user.age }}</p>
-    <p v-if="isMyUser"><b>Epost:</b> {{ user.email }}</p>
-    <p v-if="isMyUser">
-      <b>Adresse:</b> {{ user.addressLine }} {{ user.postCode }},
-      {{ user.city }}
-    </p>
-    <p v-else><b>By:</b> {{ user.city }}</p>
+    <p><b>Fornavn: </b>{{ user.firstName }}</p>
+    <p><b>Etternavn: </b>{{ user.lastName }}</p>
+    <p><b>Alder: </b>{{ user.age }}</p>
+    <p v-if="isMyUser"><b>Epost: </b>{{ user.email }}</p>
+    <p><b>By: </b>{{ user.city }}</p>
     <p>
-      <b>Allergier:</b>
+      <b>Allergier: </b>
       <span v-for="(allergy, index) in user.allergies" :key="index">
-        {{ allergy }}
+        {{ allergy
+        }}<span v-if="index != Object.keys(user.allergies).length - 1">, </span>
       </span>
     </p>
   </div>
