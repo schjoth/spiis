2e02e9e7794931e89ac0fbab965651ffae4917b2
André Schjøth <andschj@stud.ntnu.no>
2021-02-24 12:25:53 +0100

    feat(#22): viser kun redigeringsknappen for eieren av arrangementet

--- client/src/views/DinnerEvent.vue
+++ client/src/views/DinnerEvent.vue
@@ -10,7 +10,9 @@
       {{ dinner.city }}
     </p>
     <p><b>Beskrivelse: </b> {{ dinner.description }}</p>
-    <router-link :to="'/event/' + id + '/edit'"> rediger </router-link>
+    <router-link :to="'/event/' + id + '/edit'" v-if="isHost">
+      rediger
+    </router-link>
     <button type="button" v-on:click="registrerTilMiddag">
       Meld deg på middagen.
     </button>
@@ -21,18 +23,25 @@
 import { DinnerResponse } from "@/api/types";
 import { getDinner } from "@/api/dinner";
 import { useRoute } from "vue-router";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
+import { getLogInState } from "@/store/loginState";
 
 export default {
   name: "DinnerEvent",
   setup() {
-    const id = useRoute().params.dinnerId;
+    const dinnerId = useRoute().params.dinnerId;
     const dinner = ref<DinnerResponse | null>(null);
     async function fetchData() {
-      dinner.value = await getDinner(id);
+      dinner.value = await getDinner(dinnerId);
     }
+
+    const isHost = computed(
+      () => dinner.value?.host.id === getLogInState().user?.id
+    );
+
     onMounted(fetchData);
-    return { dinner, id };
+
+    return { dinner, dinnerId, isHost };
   }
 };
 </script>
