912908141ad7b0ddc95432d17913d782d3321290
André Schjøth <andschj@stud.ntnu.no>
2021-03-19 13:18:15 +0100

    fix(#77): la til nullable på annonse bilde i backend

--- server/src/spiis/server/model/Advert.java
+++ server/src/spiis/server/model/Advert.java
@@ -25,7 +25,7 @@ public class Advert {
     @Column(nullable = false)
     private String link;
 
-    @Column(columnDefinition="TEXT", )
+    @Column(columnDefinition="TEXT", nullable = false)
     private String picture;
 
     @PrePersist
