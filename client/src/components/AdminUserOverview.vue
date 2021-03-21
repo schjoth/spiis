e110c5da87b708af3e048fe91d57cbb2be4d8721
André Schjøth <andschj@stud.ntnu.no>
2021-03-21 22:52:51 +0100

    feat(#90): admin kan nå se oversikt over brukere på dashboard

---
+++ client/src/components/AdminUserOverview.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Liste over registrerte brukere</h1>
+  <ul v-if="users">
+    <li v-for="(user, index) in users" :key="index">
+      <router-link :to="'/user/' + user.id">
+        {{ user.firstName + " " + user.lastName }}</router-link
+      >
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { onMounted, ref } from "vue";
+import { UserResponse } from "@/api/types";
+import { getAllUsers } from "@/api/user";
+
+export default {
+  name: "AdminUserOverview",
+  setup() {
+    const users = ref<UserResponse[] | null>(null);
+    async function fetchData() {
+      users.value = await getAllUsers();
+    }
+    onMounted(fetchData);
+
+    return { users };
+  }
+};
+</script>
