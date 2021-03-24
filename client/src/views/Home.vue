06727a5d7a0ca0cd5e06214b5cc04bf8c830edf6
perolavbroback <perobro@stud.ntnu.no>
2021-03-24 12:35:25 +0100

    feat(#70): filtrere på sted

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,8 @@
 <template>
-  <article >
+  <article>
     <label for="city"> Velg by:</label>
-    <select
-      name="city"
-      id="city"
-      v-model="cityFilter"
-      v-if="allCities"
-    >
-      <option
-        v-for="(city, index) in allCities"
-        :key="index"
-        :value="city"
-      >
+    <select name="city" id="city" v-model="cityFilter" v-if="allCities">
+      <option v-for="(city, index) in allCities" :key="index" :value="city">
         {{ city }}
       </option>
     </select>
@@ -26,7 +17,7 @@ s
 <script lang="ts">
 import DinnerOverview from "@/components/DinnerOverview.vue";
 import { getAllDinners } from "@/api/dinner";
-import {computed, onMounted, reactive, ref} from "vue";
+import { computed, onMounted, reactive, ref } from "vue";
 import { AdvertResponse, DinnerResponse } from "@/api/types";
 import { getAllAdverts } from "@/api/adverts";
 
@@ -41,17 +32,14 @@ export default {
 
     const cityFilter = ref("Alle");
     const filteredDinners = computed(() => {
-      if (allDinners.value === null)
-        return null;
-      if (cityFilter.value === "Alle")
-        return allDinners.value;
-      return allDinners.value.filter(it => it.city === cityFilter.value);
+      if (allDinners.value === null) return null;
+      if (cityFilter.value === "Alle") return allDinners.value;
+      return allDinners.value.filter((it) => it.city === cityFilter.value);
     });
 
     const allCities = computed(() => {
-      if(allDinners.value === null)
-        return null;
-      return ["Alle", ...new Set(allDinners.value.map(it=>it.city))];
+      if (allDinners.value === null) return null;
+      return ["Alle", ...new Set(allDinners.value.map((it) => it.city))];
     });
 
     async function fetchData() {
