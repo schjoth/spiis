bc14da67c0391a4293f68186948b6e550b544f05
perolavbroback <perobro@stud.ntnu.no>
2021-03-24 12:30:00 +0100

    feat(#70): filtrere på sted

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,32 @@
 <template>
-  <article>
-    <DinnerOverview :adverts="adverts" v-if="dinners" :dinners="dinners" />
+  <article >
+    <label for="city"> Velg by:</label>
+    <select
+      name="city"
+      id="city"
+      v-model="cityFilter"
+      v-if="allCities"
+    >
+      <option
+        v-for="(city, index) in allCities"
+        :key="index"
+        :value="city"
+      >
+        {{ city }}
+      </option>
+    </select>
+    <DinnerOverview
+      :adverts="adverts"
+      v-if="filteredDinners"
+      :dinners="filteredDinners"
+    />
   </article>
 </template>
-
+s
 <script lang="ts">
 import DinnerOverview from "@/components/DinnerOverview.vue";
 import { getAllDinners } from "@/api/dinner";
-import { onMounted, ref } from "vue";
+import {computed, onMounted, reactive, ref} from "vue";
 import { AdvertResponse, DinnerResponse } from "@/api/types";
 import { getAllAdverts } from "@/api/adverts";
 
@@ -17,18 +36,35 @@ export default {
     DinnerOverview
   },
   setup() {
-    const dinners = ref<DinnerResponse[] | null>(null);
+    const allDinners = ref<DinnerResponse[] | null>(null);
     const adverts = ref<AdvertResponse[] | null>(null);
+
+    const cityFilter = ref("Alle");
+    const filteredDinners = computed(() => {
+      if (allDinners.value === null)
+        return null;
+      if (cityFilter.value === "Alle")
+        return allDinners.value;
+      return allDinners.value.filter(it => it.city === cityFilter.value);
+    });
+
+    const allCities = computed(() => {
+      if(allDinners.value === null)
+        return null;
+      return ["Alle", ...new Set(allDinners.value.map(it=>it.city))];
+    });
+
     async function fetchData() {
       const unfilteredDinners = await getAllDinners();
-      dinners.value = unfilteredDinners.filter(
+
+      allDinners.value = unfilteredDinners.filter(
         (dinner) => Date.parse(dinner.date + " " + dinner.endTime) >= Date.now()
       );
       adverts.value = await getAllAdverts();
     }
     onMounted(fetchData);
 
-    return { dinners, adverts };
+    return { adverts, cityFilter, allCities, filteredDinners };
   }
 };
 </script>
